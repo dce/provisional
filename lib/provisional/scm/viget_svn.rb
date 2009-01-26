e985d1d5a53a3fc416450e54799937f1b6ffe673
Mark Cornick <mcornick@mcornick.com>
2009-01-26 16:17:02 -0500

    remove stray command

--- lib/provisional/scm/viget_svn.rb
+++ lib/provisional/scm/viget_svn.rb
@@ -4,7 +4,6 @@ module Provisional
   module SCM
     class VigetSvn < Provisional::SCM::Svn
       def init
-        cwd = Dir.getwd
         steps = [
           "ssh -t sapporo.lab.viget.com sudo /usr/local/bin/svn-o-mat.sh #{@options[:name]}",
           "svn checkout svn://svn.lab.viget.com/#{@options[:name]}"
