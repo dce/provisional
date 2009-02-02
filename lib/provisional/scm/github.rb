1cd6550ccbe638e00ecf21476b3aca9299a06cd9
Mark Cornick <mcornick@mcornick.com>
2009-02-02 12:51:22 -0500

    correct GitHub remote

--- lib/provisional/scm/github.rb
+++ lib/provisional/scm/github.rb
@@ -1,9 +1,16 @@
 require 'provisional/scm/git'
+require 'yaml'
 
 module Provisional
   module SCM
     class Github < Provisional::SCM::Git
       def init
+        begin
+          github_config = YAML.load_file(@options[:github])
+        rescue
+          raise ArgumentError, "could not be loaded: #{@options[:github]}"
+        end
+
         steps = [
           "provisional-github-helper #{@options[:name]} #{@options[:github]}",
           "mkdir -p #{@options[:name]}",
@@ -12,7 +19,7 @@ module Provisional
           "touch .gitignore",
           "git add .gitignore",
           "git commit -m 'Structure by Provisional'",
-          "git remote add origin git.lab.viget.com:/srv/git/#{@options[:name]}.git",
+          "git remote add origin git@github.com:#{github_config['login']}/#{@options[:name]}.git",
           "git push origin master"
         ]
         steps.join(' && ')
