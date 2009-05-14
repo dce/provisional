a6655d8773207a9f12a118647453de354f2e9734
Clinton R. Nixon <crnixon@gmail.com>
2009-05-14 09:31:17 -0400

    added coverage rake tasks

--- lib/provisional/templates/viget.rb
+++ lib/provisional/templates/viget.rb
@@ -52,6 +52,22 @@ rake 'jrails:install:javascripts'
 # setup shoulda rake tasks
 file 'lib/tasks/shoulda.rake', %q[require 'shoulda/tasks']
 
+# setup rcov rake tasks
+file 'lib/tasks/coverage.rake', %q[
+namespace :test do
+  desc 'Measures test coverage'
+  task :coverage do
+    rm_f "coverage"
+    rm_f "coverage.data"
+    rcov = "rcov -Itest --rails --aggregate coverage.data -T -x \" rubygems/*,/Library/Ruby/Site/*,gems/*,rcov*\""
+    system("#{rcov} --no-html test/unit/*_test.rb test/unit/helpers/*_test.rb")
+    system("#{rcov} --no-html test/functional/*_test.rb")
+    system("#{rcov} --html test/integration/*_test.rb")
+    system("open coverage/index.html") if PLATFORM['darwin']
+  end
+end
+]
+
 # Setup testing
 file 'test/test_helper.rb', %q[
 $:.push(File.dirname(__FILE__))
